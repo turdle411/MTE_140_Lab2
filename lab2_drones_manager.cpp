b3e34bf8ddc13692f121cabd1298b1ddf468218c
Renee Yu <renyu4910@gmail.com>
2020-02-11 13:28:29 -0500

    Update lab2_drones_manager.cpp

--- lab2_drones_manager.cpp
+++ lab2_drones_manager.cpp
@@ -9,7 +9,8 @@ DronesManager::~DronesManager() {
 }
 
 bool operator==(const DronesManager::DroneRecord& lhs, const DronesManager::DroneRecord& rhs) {
-	return false;
+	return (lhs.prev == rhs.prev) && (lhs.next == rhs.next) && (lhs.droneID == rhs.droneID) 
+		&& (lhs.range == rhs.range) && (lhs.yearBought == rhs.yearBought);
 }
 
 unsigned int DronesManager::get_size() const {
@@ -21,7 +22,15 @@ bool DronesManager::empty() const {
 }
 
 DronesManager::DroneRecord DronesManager::select(unsigned int index) const {
-	return DroneRecord();
+	DroneRecord* curr = first;
+	if (curr == NULL) {
+		return DroneRecord(0);
+	} else {
+		for (int i = 1; i < (int)index && curr; i++) {
+			curr = curr->next;
+		}
+	}
+	return *curr;
 }
 
 unsigned int DronesManager::search(DroneRecord value) const {
@@ -32,7 +41,23 @@ void DronesManager::print() const {
 }
 
 bool DronesManager::insert(DroneRecord value, unsigned int index) {
-	return false;
+	if (!first || index == 0) {
+		insert_front(value);
+		return true;
+	}
+	else {
+		DroneRecord* val = new DroneRecord(value);
+		DroneRecord* curr = first;
+		for (int i = 0; i < (int)index; i++) {
+			if (!(curr->next)) {
+				return false;
+			}
+			curr = curr->next;
+		}
+		value.prev = curr->prev;
+		value.next = curr;
+		return true;
+	}
 }
 
 bool DronesManager::insert_front(DroneRecord value) {
@@ -44,7 +69,24 @@ bool DronesManager::insert_back(DroneRecord value) {
 }
 
 bool DronesManager::remove(unsigned int index) {
-	return false;
+	if (index == 0) {
+		remove_front();
+	}
+	DroneRecord* curr = first;
+	if (!curr) {
+		return false;
+	}
+	for (int i = 0; i < (int)index; i ++) {
+		if (!(curr->next)) {
+			remove_back();	
+			return true;	
+		}
+	}
+	DroneRecord* back = curr->prev;
+	back->next = curr->next;
+	delete curr;
+	curr = NULL;
+	return true;
 }
 
 bool DronesManager::remove_front() {
@@ -56,7 +98,22 @@ bool DronesManager::remove_back() {
 }
 
 bool DronesManager::replace(unsigned int index, DroneRecord value) {
-	return false;
+	if (!first) {
+		return false;	
+	}
+	else {
+		DroneRecord* curr = first;
+		for (int i = 0; i < (int)index && curr; i++) {
+			if (!curr -> next) {
+				return false;
+			}
+			curr = curr->next;
+			
+		}
+		*curr = value;
+
+		return true;
+	}
 }
 
 bool DronesManager::reverse_list() {
